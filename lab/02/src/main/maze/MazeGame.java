6067dab15344d89d0ba5b18e12b6e6317f7ddbca
TheTeaCup <lw0482352@gmail.com>
2024-01-24 18:00:54 -0500

    ++

---
+++ lab/02/src/main/maze/MazeGame.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MazeGame {
+    public static int HEIGHT;
+    public static int WIDTH;
+    private static int COL;
+    private static int ROW;
+    private Scanner playerInput;
+    private boolean[][] blocked;
+    private boolean[][] visited;
+    private int[] player;
+    private int[] goal;
+    private int[] start;
+
+    public MazeGame(String mazeFile) {
+
+    }
+
+    public MazeGame(String mazeFile, Scanner playerInput) {
+
+    }
+
+    public void playGame() {
+        
+    }
+}
