0d1e8e62fb01adc9d9e298ff26a96fe9c5ed89f0
TheTeaCup <lw0482352@gmail.com>
2024-01-31 12:19:17 -0500

    start of lab 3

---
+++ lab/03/src/main/algorithms/BigOh.java
@@ -0,0 +1,45 @@
+package algorithms;
+
+import java.util.Random;
+
+public class BigOh {
+    private static double MILLISECONDS_PER_SECOND;
+    private static int NUM_TRIALS;
+    private Random rand;
+
+    public BigOh() {
+
+    }
+
+    public BigOh(Random rand) {
+
+    }
+
+    public int runAlgorithm(int choice, int numElements) {
+        return 1;
+    }
+
+    public double bigOhFunc(int choice, double n) {
+        return 0.0;
+    }
+
+    public double timeAlgorithm(int choice, int n) {
+        return 0.0;
+    }
+
+    public double robustTimeAlgorithm(int choice, int n) {
+        return 0.0;
+    }
+
+    public double estimateTiming(int choice, int n1, double t1, int t2) {
+        return 0.0;
+    }
+
+    public double percentError(double correct, double edtimate) {
+        return 0.0;
+    }
+
+    public double computePercentError(int choice, int n1, int n2) {
+        return 0.0;
+    }
+}
